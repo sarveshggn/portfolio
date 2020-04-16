0282be8dd36b36f2a9f3ee0ab02e6e4258b4d782
sarvesh joshi <sarveshggn@gmail.com>
2020-04-16 21:28:25 +0530

    Created home page added bootstrap and jobs almost fin

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='image/')
