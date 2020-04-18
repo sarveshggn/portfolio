655fab15b0e80f4388c2bb23e34be09966833bc2
sarvesh joshi <sarveshggn@gmail.com>
2020-04-18 13:51:07 +0530

    changed database

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfoliodb',
-        'USER': 'postgres',
-        'PASSWORD': 'dobbyisfree',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
