b940363edce85295db394ed97383a30acd56db81
sarvesh joshi <sarveshggn@gmail.com>
2020-07-25 22:38:36 +0530

    added links

--- dsproj/models.py
+++ dsproj/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Dsproject(models.Model):
     image = models.ImageField(upload_to='image/')
     summary = models.CharField(max_length=255)
+    github_link = models.CharField(max_length=300, default='#')
 
     def __str__(self):
         return self.summary
